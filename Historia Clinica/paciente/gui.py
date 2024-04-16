d99145418263bcfd40bd3d4c19d43c52a146136f
Alchagiro23 <acgtecnologias@gmail.com>
2024-04-15 19:18:49 -0500

    funciones y acomodo de los entrys

--- Historia Clinica/paciente/gui.py
+++ Historia Clinica/paciente/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from modelo.pacienteDao import Persona, guardarDatoPaciente
 from tkinter import font
 
 class Frame(tk.Frame):
@@ -9,6 +10,7 @@ class Frame(tk.Frame):
         self.pack()
         self.config(bg='#CDD8FF')
         self.camposPaciente()
+
 
 # LABELS
     def camposPaciente(self):  #
@@ -56,57 +58,68 @@ class Frame(tk.Frame):
         self.entryNombre.grid(column=1, row=0, padx=10, pady=5, columnspan=2)
 
         self.svApePaterno = tk.StringVar()
-        self.svApePaterno = tk.Entry(self, textvariable=self.svApePaterno)
-        self.svApePaterno.config(width=50, font=('ARIAL',15))
-        self.svApePaterno.grid(column=1, row=1, padx=10, pady=5, columnspan=2)
+        self.entryApePaterno = tk.Entry(self, textvariable=self.svApePaterno)
+        self.entryApePaterno.config(width=50, font=('ARIAL',15))
+        self.entryApePaterno.grid(column=1, row=1, padx=10, pady=5, columnspan=2)
 
         self.svApeMaterno = tk.StringVar()
-        self.svApeMaterno = tk.Entry(self, textvariable=self.svApeMaterno)
-        self.svApeMaterno.config(width=50, font=('ARIAL',15))
-        self.svApeMaterno.grid(column=1, row=2, padx=10, pady=5, columnspan=2)
+        self.entryApeMaterno = tk.Entry(self, textvariable=self.svApeMaterno)
+        self.entryApeMaterno.config(width=50, font=('ARIAL',15))
+        self.entryApeMaterno.grid(column=1, row=2, padx=10, pady=5, columnspan=2)
 
         self.svCedula = tk.StringVar()
-        self.svCedula = tk.Entry(self, textvariable=self.svCedula)
-        self.svCedula.config(width=50, font=('ARIAL',15))
-        self.svCedula.grid(column=1, row=3, padx=10, pady=5, columnspan=2)
+        self.entryCedula = tk.Entry(self, textvariable=self.svCedula)
+        self.entryCedula.config(width=50, font=('ARIAL',15))
+        self.entryCedula.grid(column=1, row=3, padx=10, pady=5, columnspan=2)
 
         self.svFechNacimiento = tk.StringVar()
-        self.svFechNacimiento = tk.Entry(self, textvariable=self.svFechNacimiento)
-        self.svFechNacimiento.config(width=50, font=('ARIAL',15))
-        self.svFechNacimiento.grid(column=1, row=4, padx=10, pady=5, columnspan=2)
+        self.entryFechNacimiento = tk.Entry(self, textvariable=self.svFechNacimiento)
+        self.entryFechNacimiento.config(width=50, font=('ARIAL',15))
+        self.entryFechNacimiento.grid(column=1, row=4, padx=10, pady=5, columnspan=2)
 
         self.svEdad = tk.StringVar()
-        self.svEdad = tk.Entry(self, textvariable=self.svEdad)
-        self.svEdad.config(width=50, font=('ARIAL',15))
-        self.svEdad.grid(column=1, row=5, padx=10, pady=5, columnspan=2)
+        self.entryEdad = tk.Entry(self, textvariable=self.svEdad)
+        self.entryEdad.config(width=50, font=('ARIAL',15))
+        self.entryEdad.grid(column=1, row=5, padx=10, pady=5, columnspan=2)
 
         self.svAntecedentes = tk.StringVar()
-        self.svAntecedentes = tk.Entry(self, textvariable=self.svAntecedentes)
-        self.svAntecedentes.config(width=50, font=('ARIAL',15))
-        self.svAntecedentes.grid(column=1, row=6, padx=10, pady=5, columnspan=2)
+        self.entryAntecedentes = tk.Entry(self, textvariable=self.svAntecedentes)
+        self.entryAntecedentes.config(width=50, font=('ARIAL',15))
+        self.entryAntecedentes.grid(column=1, row=6, padx=10, pady=5, columnspan=2)
 
         self.svCorreo  = tk.StringVar()
-        self.svCorreo = tk.Entry(self, textvariable=self.svAntecedentes)
-        self.svCorreo.config(width=50, font=('ARIAL',15))
-        self.svCorreo.grid(column=1, row=7, padx=10, pady=5, columnspan=2)
+        self.entryCorreo = tk.Entry(self, textvariable=self.svCorreo)
+        self.entryCorreo.config(width=50, font=('ARIAL',15))
+        self.entryCorreo.grid(column=1, row=7, padx=10, pady=5, columnspan=2)
 
         self.svTelefono  = tk.StringVar()
-        self.svTelefono = tk.Entry(self, textvariable=self.svAntecedentes)
-        self.svTelefono.config(width=50, font=('ARIAL',15))
-        self.svTelefono.grid(column=1, row=8, padx=10, pady=5, columnspan=2)
+        self.entryTelefono = tk.Entry(self, textvariable=self.svTelefono)
+        self.entryTelefono.config(width=50, font=('ARIAL',15))
+        self.entryTelefono.grid(column=1, row=8, padx=10, pady=5, columnspan=2)
 
         #BUTTONS
         self.btnNuevo = tk.Button(self, text='Nuevo')
         self.btnNuevo.config(width=20, font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#158645', cursor='hand2',activebackground='#35BD6F')
         self.btnNuevo.grid(column=0, row=9, padx=10, pady=5)
 
-        self.btnGuardar = tk.Button(self, text='Guardar')
+        self.btnGuardar = tk.Button(self, text='Guardar', command=self.guardarPaciente)
         self.btnGuardar.config(width=20, font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#0475C6', cursor='hand2',activebackground='#68B3E9')
         self.btnGuardar.grid(column=1, row=9, padx=10, pady=5)
 
         self.btnCancelar = tk.Button(self, text='Cancelar')
         self.btnCancelar.config(width=20, font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#B00000', cursor='hand2',activebackground='#D27C7C')
         self.btnCancelar.grid(column=2, row=9, padx=10, pady=5)
+
+        
+    def guardarPaciente(self):
+        persona = Persona(
+            self.svNombre.get(), self.svApePaterno.get(), self.svApeMaterno.get(), self.svCedula.get(), self.svFechNacimiento.get(), self.svEdad.get(), self.svAntecedentes.get(), self.svCorreo.get(), self.svTelefono.get()
+        )
+        guardarDatoPaciente(persona)
+
+    def deshabilitar(self):
+        self.entryNombre.config(state='disable')
+        self.en
 
         
 
